7539b1f26bbae8f50015712189b0f17dd7913b59
Paul Greenham <paul.greenham@gmail.com>
2019-05-27 23:10:26 +0300

    added rooms, still getting it to work

--- server.js
+++ server.js
@@ -14,25 +14,49 @@ app.use(express.static(path.join(__dirname, 'build')))
 // app.use(express.static(path.join(__dirname, 'node_modules')))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
-// app.use(function (req, res, next) {
-//     res.header('Access-Control-Allow-Origin', '*')
-//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
-//     next()
-// })
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
+    next()
+})
 app.use('/', api)
 
 
-const port = 3000
+const port = 8000
 const server = app.listen(port, function () {
     console.log(`Server running on port ${port}`)
 })
 
 const io = socketIo(server)
+const rooms = []
 
 io.on("connection", function(socket) {
     console.log("New connection on socket id:" + socket.id)
-    socket.on('msg-to-server', function (message) {
-        io.emit('msg-from-server', message)
+    socket.emit('rooms-to-client', rooms)
+
+    let room
+
+    socket.on('join-room', function (joinData) {
+        room = joinData.room
+        socket.join(room)
+        io.sockets.in(room).emit('new-join', `${joinData.name} has joined room: ${room}`)
     })
+
+    socket.on('create-room', function (roomName) {
+        room = roomName
+        rooms.push(room)
+        socket.join(room)
+    })
+
+    socket.on('msg-to-server', function (message) {
+        io.sockets.in(room).emit('msg-from-server', message)
+    })
+
+    // socket.on('disconnect', function () {
+    //     socket.leave(room)
+    //     if (io.sockets.adapter.rooms[room] && io.sockets.adapter.rooms[room].length < 1) {
+    //         io.sockets.adapter.rooms[room] = null
+    //     }
+    // })
 })
--- src/App.js
+++ src/App.js
@@ -8,15 +8,23 @@ class App extends Component {
     super()
     this.state = {
       username: "",
+      roomName: "",
+      joinSelection: "",
+      inRoom: false,
       message: "",
-      socket: openSocket('/'),
-      messages: []
+      socket: openSocket('http://localhost:8000'),
+      messages: [],
+      rooms: []
     }    
   }
 
   changeName = event => this.setState({ username: event.target.value })
 
   changeMessage = event => this.setState({ message: event.target.value })
+
+  changeRoomName = event => this.setState({ roomName: event.target.value })
+
+  joinInput = event => this.setState({ joinSelection: event.target.value })
 
   updateMessages = () => {
     this.state.socket.on('msg-from-server', message => {
@@ -26,12 +34,60 @@ class App extends Component {
     })
   }
 
+  getRooms = () => {
+    this.state.socket.on('rooms-to-client', rooms => {
+      this.setState({ rooms: rooms })
+    })
+  }
+
+  createAndJoinRoom = () => {
+    this.state.socket.emit('create-room', this.state.roomName)
+    this.setState({ inRoom: true })
+  }
+
   sendMessage = () => {
     this.state.socket.emit('msg-to-server', { name: this.state.username, message: this.state.message })
   }
 
+  joinRoom = () => {
+    this.state.socket.emit('join-room', {name: this.state.username, room: this.state.joinSelection})
+    this.setState({ 
+      inRoom: true,
+      roomName: this.state.joinSelection
+    })
+  }
+
+  chooseRoom = () => {
+    return (<React.Fragment>
+      <select value={this.state.joinSelection} onChange={this.joinInput}>
+        <option value="">Choose a currently active Room:</option>
+        {this.state.rooms.map((r, i) => <option value={r} key={i}>{r}</option>)}
+      </select>
+      <button onClick={this.joinRoom}>Join room</button>
+    </React.Fragment>)
+  }
+
+  renderRoomSelection = () => {
+    this.getRooms()
+    return (<div>
+          {this.state.rooms.length > 0 ? this.chooseRoom() : null}
+          <input type="text" placeholder="Room name" value={this.state.roomName} onChange={this.changeRoomName}/>
+          <button onClick={this.createAndJoinRoom}>Create new room</button>
+      </div>)
+  }
+
+  renderChats = () => {
+    return (<div>
+        <div>Room: {this.state.roomName} </div>
+        <input type="text" value={this.state.message} onChange={this.changeMessage}/>
+        <button onClick={this.sendMessage}>Post</button>
+        {this.state.messages.map((m, i) => <div key={i}>{m.name}: {m.message}</div>)}
+    </div>)
+  }
+
   componentDidMount() {
     this.updateMessages()
+    this.state.socket.on('new-join', message => alert(message))
   }
 
 
@@ -39,9 +95,7 @@ class App extends Component {
     return (
       <div className="App">
         <input type="text" value={this.state.username} onChange={this.changeName} placeholder="enter your name"/>
-        <input type="text" value={this.state.message} onChange={this.changeMessage}/>
-        <button onClick={this.sendMessage}>Post</button>
-        {this.state.messages.map((m, i) => <div key={i}>{m.name}: {m.message}</div>)}
+        {this.state.inRoom ? this.renderChats() : this.renderRoomSelection()}
       </div>
     )
   }
